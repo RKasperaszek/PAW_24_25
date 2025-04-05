24ac78e5b7158b284bdccfe17e45d575c67a7f6e
RKasperaszek <kasperaszekroksana@gmail.com>
2025-04-05 20:52:33 +0200

    Zadania 18.11.24, 4.12, 11.12, 18.12, 3.02

--- 18_11_24/my-react-app/src/main.tsx
+++ 18_11_24/my-react-app/src/main.tsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import './index.sass'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
+import React from 'react';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </QueryClientProvider>
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </QueryClientProvider>
+    </React.StrictMode>,
 );
 
--- 18_11_24/my-react-app/vite.config.ts
+++ 18_11_24/my-react-app/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src', // Optional alias, ensure paths are correctly resolved
+    },
+  },
+  server: {
+    port: 3000, // Ensure the server starts without conflicts
+  },
 })
